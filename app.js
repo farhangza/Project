76943aa8db21a837ad4a01713c9e5716eca81897
farhang <farhangza@gmail.com>
2015-04-04 17:20:55 -0400

    App.js added to the project, The passport part is copied from the internet, I just edited them! --added an icon :D
    Signed-off-by: farhang <farhangza@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,68 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + " W3Schools. All rights reserved.</p>";
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='Index.html'>Home</a></li>" +
-"<li><a href='Customers.html'>Data</a></li>" +
-"<li><a href='About.html'>About</a></li>" +
-"</ul>";
+//required elements
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var cookieParser = require('cookie-parser');var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+
+// USing jade,....
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', routes);
+
+// Options required by passport
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// Online mongoose data-base
+mongoose.connect('mongodb://Farhangza:FArhang90@ds059471.mongolab.com:59471/farhang');
+
+//Different errors are returned in here including 404,500,....
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+module.exports = app;
